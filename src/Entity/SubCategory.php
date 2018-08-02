786ba7750d604099f0e71e0bf57b2812a7d55c64
Andrey Shamis <lolnik@gmail.com>
2018-08-02 17:13:06 +0300

    Change subCut table name
    Change-Id: I5cae91656b7f6c64a34b68a5a0bf03016387d418
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Entity/SubCategory.php
+++ src/Entity/SubCategory.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="ec_sub_category")
  * @ORM\Entity(repositoryClass="App\Repository\SubCategoryRepository")
  */
 class SubCategory
